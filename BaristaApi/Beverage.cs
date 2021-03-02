1cecee4e2bb27aa124a9513176d20a7e73e8b3e9
SimonMachhabhai <Simon_96m@hotmail.com>
2021-03-02 14:36:52 +0100

    Kom in matchen
    Testteststestsetestest

--- BaristaApi/Beverage.cs
+++ BaristaApi/Beverage.cs
@@ -10,11 +10,16 @@ class Espresso : IBeverage
     public List<string> Ingredients => throw new System.NotImplementedException();
 
     public string CupType => throw new System.NotImplementedException();
-    //FDBMNNBKRTBMÖBARE
+    //FDBMNNBKRTBMï¿½BARE
     //HJEHEJEHEJEJ
     //HEJHEJ
     //This is a test.
     //fgbaedrfbaerbe
+    //
+    //
+    //
+    //
+    //Kom in i matchen boys. 
 }
 
 class Latte : IBeverage
